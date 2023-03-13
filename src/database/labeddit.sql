f1fee5005e872743bc3e0802f7e2ed3830c538ce
mari-trancoso <mnegraotrancoso@gmail.com>
2023-03-13 16:30:42 -0300

    Criado endpoint de Signup.

---
+++ src/database/labeddit.sql
@@ -0,0 +1,72 @@
+-- Active: 1678207108745@@127.0.0.1@3306
+
+CREATE TABLE users (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    nickname TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL,
+    created_at TEXT DEFAULT (DATETIME()) NOT NULL
+);
+
+INSERT INTO users (id, nickname, email, password, role)
+VALUES
+	("u001", "Fulano", "fulano@email.com", "fulano123", "NORMAL"),
+	("u002", "Beltrana", "beltrana@email.com", "beltrana00", "NORMAL"),
+	("u003", "Astrodev", "astrodev@email.com", "astrodev99", "ADMIN");
+
+CREATE TABLE posts (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    creator_id TEXT NOT NULL,
+    content TEXT NOT NULL,
+    likes INTEGER DEFAULT (0) NOT NULL,
+    dislikes INTEGER DEFAULT (0) NOT NULL,
+    created_at TEXT DEFAULT (DATETIME()) NOT NULL,
+    updated_at TEXT DEFAULT (DATETIME()) NOT NULL,
+    comments INTEGER DEFAULT (0) NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE comments (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    creator_id TEXT NOT NULL,
+    post_id TEXT NOT NULL,
+    content TEXT NOT NULL,
+    likes INTEGER DEFAULT (0) NOT NULL,
+    dislikes INTEGER DEFAULT (0) NOT NULL,
+    created_at TEXT DEFAULT (DATETIME()) NOT NULL,
+    updated_at TEXT DEFAULT (DATETIME()) NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES posts(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE post_likes_dislikes (
+    user_id TEXT NOT NULL,
+    post_id TEXT NOT NULL,
+    like INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES posts (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE comment_likes_dislikes (
+    user_id TEXT NOT NULL,
+    comment_id TEXT NOT NULL,
+    like INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (comment_id) REFERENCES comments (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
