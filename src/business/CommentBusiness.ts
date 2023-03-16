b01b78c43cf878d53f1ffe2bb4e8a9c05b73e7e8
mari-trancoso <mnegraotrancoso@gmail.com>
2023-03-16 12:48:57 -0300

    Endpoint de Get Comments implementado.

--- src/business/CommentBusiness.ts
+++ src/business/CommentBusiness.ts
@@ -1,6 +1,10 @@
 import { CommentDatabase } from "../database/CommentDatabase";
+import { GetCommentsInputDTO, GetCommentsOutputDTO } from "../dtos/commentDTO";
+import { BadRequestError } from "../errors/BadRequestError";
+import { Comment } from "../models/Comment";
 import { IdGenerator } from "../services/IdGenerator";
 import { TokenManager } from "../services/TokenManager";
+import { CommentDB } from "../types";
 
 export class CommentBusiness {
     constructor(
@@ -8,4 +12,39 @@ export class CommentBusiness {
         private idGenerator: IdGenerator,
         private tokenManager: TokenManager
     ) { }
+
+    public getComments = async (input: GetCommentsInputDTO): Promise<GetCommentsOutputDTO> => {
+        const { token, id } = input
+
+        if (token === undefined) {
+            throw new BadRequestError("'token' ausente")
+        }
+
+        const payload = this.tokenManager.getPayload(token)
+
+        if (payload === null) {
+            throw new BadRequestError("'token' inválido")
+        }
+
+        const commentsWithPostDB: CommentDB[] = await this.commentDatabase.commentsWithPost()
+
+        const comments = commentsWithPostDB.map((commentWithPostDB) => {
+            const comment = new Comment(
+                commentWithPostDB.id,
+                commentWithPostDB.creator_id,
+                commentWithPostDB.post_id,
+                commentWithPostDB.content,
+                commentWithPostDB.likes,
+                commentWithPostDB.dislikes,
+                commentWithPostDB.created_at,
+                commentWithPostDB.updated_at
+            )
+
+            return comment.toBusinessModel()
+        })
+
+        const output: GetCommentsOutputDTO = comments
+
+        return output
+    }
 }
--- src/database/CommentDatabase.ts
+++ src/database/CommentDatabase.ts
@@ -1,7 +1,30 @@
+import { CommentDB } from "../types"
 import { BaseDatabase } from "./BaseDatabase"
 
 export class CommentDatabase extends BaseDatabase {
     public static TABLE_COMMENTS = "comments"
     public static TABLE_POSTS = "posts"
+    public static TABLE_USERS = "users"
     public static TABLE_COMMENTS_LIKES_DISLIKES = "comment_likes_dislikes"
+
+    public commentsWithPost = async () => {
+        const result: CommentDB[] = await BaseDatabase
+            .connection(CommentDatabase.TABLE_COMMENTS)
+            .select(
+                "comments.id",
+                "comments.creator_id",
+                "comments.post_id",
+                "comments.content",
+                "comments.likes",
+                "comments.dislikes",
+                "comments.created_at",
+                "comments.updated_at",
+                "users.nickname AS creator_nickname",
+                "posts.id AS post_id"
+            )
+            .join("users", "comments.creator_id", "=", "users.id")
+            .join("posts", "comments.post_id", "=", "posts.id")
+
+            return result
+    }
 }
